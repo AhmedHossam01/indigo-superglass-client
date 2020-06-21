cfea18bf005cdf1c9bf345a03055f447c9e872f9
AhmedHossam01 <50014916+AhmedHossam01@users.noreply.github.com>
2020-06-21 10:03:04 +0200

    Navbar #1: Finishing prototype for Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,17 +3,48 @@ import "./navbar.scss";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import Icon from "@mdi/react";
+import { mdiAccount, mdiCart, mdiMagnify } from "@mdi/js";
 
 const AppNavbar = () => {
   return (
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+        <Navbar.Brand className="mr-0" href="/">
+          <img src="images/logo.svg" className="navbar__logo" alt="indigo" />
+        </Navbar.Brand>
+
+        <Navbar.Toggle
+          className="navbar__toggler"
+          aria-controls="basic-navbar-nav"
+        />
+
+        <Navbar.Collapse>
+          <Nav className="ml-auto nav">
+            <a className="nav__link nav__link--active" href="/">
+              Home
+            </a>
+            <a className="nav__link" href="/">
+              Shop
+            </a>
+            <a className="nav__link" href="/">
+              Contact
+            </a>
+            <a className="nav__link" href="/">
+              About
+            </a>
+          </Nav>
+
+          <Nav className="ml-auto nav__icons-container">
+            <a className="nav__icon" href="/">
+              <Icon path={mdiAccount} size={1} color="#272727" />
+            </a>
+            <a className="nav__icon" href="/">
+              <Icon path={mdiCart} size={1} color="#272727" />
+            </a>
+            <a className="nav__icon" href="/">
+              <Icon path={mdiMagnify} size={1} color="#272727" />
+            </a>
           </Nav>
         </Navbar.Collapse>
       </Container>
